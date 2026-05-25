69fa7151f637676c759438a0716fdd1cd3d99879
kamilihamzah <hamzah.kamil97@gmail.com>
2026-05-25 17:31:19 +0530

    Add files via upload

---
+++ sdc.js
@@ -0,0 +1,11 @@
+
+// --- DISCOUNT CONFIGURATION ---
+// Moving this here bypasses the file:/// security block!
+window.DISCOUNT_CONFIG = { 
+    quick:   { code: "QUICK20",  usd_percent: 20, inr_percent: 15 },
+    kash:    { code: "KASH40",  usd_percent: 40, inr_percent: 30 },  
+    iran:    { code: "IRAN786", usd_percent: 60, inr_percent: 50 },
+    special: { code: "OFFER5",  usd_percent: 5,  inr_percent: 10 },
+    mega:    { code: "FREE100", usd_percent: 0,  inr_percent: 0 },
+    Rsb:     { code: "RSB", usd_percent: 95, inr_percent: 95 }
+};
